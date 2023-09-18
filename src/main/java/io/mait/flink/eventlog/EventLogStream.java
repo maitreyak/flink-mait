cc81fa36ea9bc51e8f435b84f92f2b07c3253cf4
mk <mk@mks-macbook-pro.lan>
2023-09-18 07:05:36 -0500

    added support from the IDE to use Minio as the object store

--- src/main/java/io/mait/flink/eventlog/EventLogStream.java
+++ src/main/java/io/mait/flink/eventlog/EventLogStream.java
@@ -3,30 +3,36 @@ package io.mait.flink.eventlog;
 import io.mait.flink.avro.EventLog;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.avro.generic.GenericRecord;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.connector.file.sink.FileSink;
+import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.formats.parquet.avro.AvroParquetWriters;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.OnCheckpointRollingPolicy;
 
-import java.util.UUID;
 
 @Slf4j
 public class EventLogStream {
     public static void main(String[] args) throws Exception {
+
+        Configuration configuration = new Configuration();
+        configuration.setString("s3.endpoint", "http://127.0.0.1:9000");
+        configuration.setString("s3.access-key", "minioadmin");
+        configuration.setString("s3.secret-key", "minioadmin");
+        FileSystem.initialize(configuration);
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
         env.enableCheckpointing(1000);
-        env.getCheckpointConfig().setCheckpointStorage("s3://flink/checkpoint-dir");
+        env.getCheckpointConfig().setCheckpointStorage("s3://tmp/checkpoint-dir");
         DataStream<GenericRecord> eventLogDataStream = env.addSource(new EventLogSource());
         final FileSink<GenericRecord> psink = FileSink
-                .forBulkFormat(new Path("s3://flink/something"), AvroParquetWriters.forGenericRecord(EventLog.getClassSchema()))
+                .forBulkFormat(new Path("s3://tmp/something"), AvroParquetWriters.forGenericRecord(EventLog.getClassSchema()))
                 .withRollingPolicy(OnCheckpointRollingPolicy.build())
                 .build();
-        UUID uuid = UUID.randomUUID();
-        log.info("job name is {}", uuid);
-        eventLogDataStream.sinkTo(psink).uid("sink_me_to_hell");
+        eventLogDataStream.sinkTo(psink);
         env.execute("File writer");
+
     }
 }
